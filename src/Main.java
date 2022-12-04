4c1cb9ba46d2820607acf489febb661498fe3112
dimountain <gftght@rambler.ru>
2022-12-05 01:19:08 +0300

    выполнил задачу 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
+        int a = 17838;
+        byte b = -125;
+        short c = 32374;
+        long d = 123456789;
+        float e = 3.3437557f;
+        double f = -56.654434365d;
+        System.out.println("Задача 1");
+        System.out.println("Значение переменной a с типом int равно " + a);
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        System.out.println("Значение переменной c с типом short равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
     }
 }
